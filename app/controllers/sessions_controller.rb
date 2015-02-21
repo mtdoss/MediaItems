007b3a3a3b213e9aa0226d35e6aa17885f84968d
Mark <mtdoss@gmail.com>
2015-02-21 13:07:17 -0800

    Added session logic

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  before_filter :require_current_user!, only: [:destroy]
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user.nil?
+      @user = User.new
+      render :new
+    else
+      login_user!
+    end
+  end
+
+  def destroy
+    current_user.reset_session_token!
+    self.session[:session_token] = nil
+
+    redirect_to new_session_url
+  end
+end
