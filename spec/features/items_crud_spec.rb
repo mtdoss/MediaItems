033a5a5957577c5cda7058f98a1e74c6b2bcd4ee
Mark <mtdoss@gmail.com>
2015-02-21 15:19:22 -0800

    I think capybara needs the label id, not the name id

--- spec/features/items_crud_spec.rb
+++ spec/features/items_crud_spec.rb
@@ -25,13 +25,13 @@ feature "CRUD of items" do
     it 'should have an edit item page' do
       visit edit_item_url(item)
       expect(page).to have_content "Edit item"
-      find_field('item[title]').value.should eq item.title
-      find_field('item[url]').value.should eq item.url 
+      find_field('title').value.should eq item.title
+      find_field('url').value.should eq item.url 
     end
 
     it 'should show the updated item after edit is saved' do
       visit edit_item_url(item)
-      fill_in "item[title]", with: "Silly dog"
+      fill_in "title", with: "Silly dog"
       click_button "Update Item"
       expect(page).not_to have_content "Edit item"
       expect(page).to have_content "Silly dog"
--- spec/support/item_features_helper.rb
+++ spec/support/item_features_helper.rb
@@ -1,13 +1,13 @@
 module ItemFeaturesHelper
   def submit_new_item(item_title, item_url, item_type, privacy = {private: false} )
     visit new_item_url
-    fill_in "item[title]", with: item_title
+    fill_in "title", with: item_title
     if privacy[:private]
-      check "item[private]"
+      check "private"
     end
 
-    fill_in "item[url]", with: item_url
-    fill_in "item[ttype]", with: item_type
+    fill_in "url", with: item_url
+    fill_in "ttype", with: item_type
 
     click_button "New Item"
   end
